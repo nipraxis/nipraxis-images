18aa972b6ce770008571fcc56ecb9a1ad1d35119
Matthew Brett <matthew.brett@gmail.com>
2022-03-16 17:25:17 +0000

    Trying to make minimal notebook work with Theia

---
+++ nibabel-theia/build_scripts/build_theia.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# From within given directory.
+# Directory must contain package.json file.
+yarn --pure-lockfile && \
+    NODE_OPTIONS="--max_old_space_size=4096" yarn theia build && \
+    yarn theia download:plugins && \
+    yarn --production && \
+    yarn autoclean --init && \
+    echo *.ts >> .yarnclean && \
+    echo *.ts.map >> .yarnclean && \
+    echo *.spec.* >> .yarnclean && \
+    yarn autoclean --force && \
+    yarn cache clean
